f9f2d48194255ac81b3a23ae98688c9a09050439
Tyler Black <blacktylera@gmail.com>
2015-02-13 15:48:43 -0600

    successfully linked foundation. adding test styles.

--- build.sh
+++ build.sh
@@ -17,7 +17,8 @@ mkdir public/vendor
 cp bower_components/jquery/dist/jquery.min.js public/vendor
 cp bower_components/lodash/lodash.min.js public/vendor
 cp bower_components/firebase/firebase.js public/vendor
-
+cp bower_components/foundation/css/foundation.css public/vendor
+cp bower_components/foundation/css/normalize.css public/vendor
 
 ###############################
 # Jade (npm intalled locally) #
