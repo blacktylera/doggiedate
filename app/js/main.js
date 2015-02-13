5f4d3899f7075f9b44527aefaaed4ecb321eaf7f
Andy Meagher <ameagher311@gmail.com>
2015-02-13 15:38:44 -0600

    got login and register working with firebase and linking to profile and register page

---
+++ app/js/main.js
@@ -0,0 +1,55 @@
+var FIREBASE_URL   = 'https://doggie-date.firebaseio.com',
+    fb             = new Firebase(FIREBASE_URL),
+    usersFb;
+
+ if (fb.getAuth()) {
+  usersFb = fb.child('users/' + fb.getAuth().uid);
+   }
+
+ $('.login input[type="button"]').click(function (event) {
+  var $loginForm = $('.loginForm'),
+      email      = $loginForm.find('[type="email"]').val(),
+      pass       = $loginForm.find('[type="password"]').val(),
+      data       = {email: email, password: pass};
+
+   registerAndLogin(data, function (err, auth) {
+    if (err) {
+      $('.error').text(err);
+    } else {
+      window.location.href='./test.html';
+    }
+  });
+});
+
+ $('.login form').submit(function(event){
+  var $loginForm = $(event.target),
+      email      = $loginForm.find('[type="email"]').val(),
+      pass       = $loginForm.find('[type="password"]').val(),
+      data       = {email: email, password: pass};
+
+  event.preventDefault();
+
+  fb.authWithPassword(data, function(err, auth) {
+		    if (err) {
+      $('.error').text(err);
+    } else {
+      window.location.href='./profile.html';
+    }
+  });
+});
+
+function registerAndLogin(obj, cb) {
+  fb.createUser(obj, function(err) {
+		    if (!err) {
+    			  fb.authWithPassword(obj, function (err, auth){
+        if (!err) {
+          cb(null, auth);
+        } else {
+          cb(err);
+        }
+      });
+    } else {
+      cb(err);
+    }
+  });
+}
